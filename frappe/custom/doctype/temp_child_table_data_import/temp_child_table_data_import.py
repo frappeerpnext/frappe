120223c4ccf9fdfd8d704e6a6ac3448f56bc4016
frappeerpnext <frapperpnext@gmail.com>
2022-07-28 09:39:27 +0700

    upate api reset data

--- frappe/custom/doctype/temp_child_table_data_import/temp_child_table_data_import.py
+++ frappe/custom/doctype/temp_child_table_data_import/temp_child_table_data_import.py
@@ -9,11 +9,12 @@ class TempChildTableDataImport(Document):
 	def after_insert(self):
 		if self.parent_doctype=="Item" and self.doctype_name=="Item Barcode":
 			add_barcode_to_item(self)
+		elif self.parent_doctype=="Item" and self.doctype_name=="UOM Conversion Detail":
+			add_unit_to_item(self)
 
 
 def add_barcode_to_item(self):
 	doc = frappe.get_doc(self.parent_doctype,self.doc_name)
-	doc.barcodes
 	doc.append("barcodes", {
                 "barcode":self.barcode,
                 "uom":self.uom,
@@ -21,4 +22,14 @@ def add_barcode_to_item(self):
 	doc.save()
 	frappe.db.commit()
 
-	
+
+def add_unit_to_item(self):
+	doc = frappe.get_doc(self.parent_doctype,self.doc_name)
+	doc.append("uoms", {
+                "conversion_factor":self.conversion_factor,
+                "uom":self.uom,
+            })
+	doc.save()
+	frappe.db.commit()
+
+
