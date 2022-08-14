e0fda78d934bda0987e8570d45dcce602b3c2a2b
frappeerpnext <frapperpnext@gmail.com>
2022-08-14 13:59:29 +0700

    update data for sync

--- frappe/custom/doctype/data_for_sync/data_for_sync.py
+++ frappe/custom/doctype/data_for_sync/data_for_sync.py
@@ -9,15 +9,22 @@ class DataforSync(Document):
 
 @frappe.whitelist()
 def notify_sync(doc, event):
-	notify_docs = ["Item","Item Group","Item Price","Customer","Customer Group" ,"User","POS Profile","Company","System Settings","Currency Exchange","Warehouse" ,"Membership Type"]
-	"""called via hooks"""
-	if doc.doctype in notify_docs:
-	 	 notify_sync_job(doc.doctype,doc.name,event)
+	settings = frappe.get_doc('System Settings')
+	if not settings.disable_data_for_sync_notify:
+		notify_docs = ["Item","Item Group","Item Price","Customer","Customer Group" ,"User","POS Profile","Company","System Settings","Currency Exchange","Warehouse" ,"Membership Type"]
+		"""called via hooks"""
+		if doc.doctype in notify_docs:
+			if doc.doctype=='Item Price':
+				notify_sync_job("Item",doc.item_code,event)
+			else:
+				notify_sync_job(doc.doctype,doc.name,event)
 
 
 @frappe.whitelist()
 def notify_sync_job(doctype,name, event):
 	
+	
+
 	
 	branches = frappe.db.get_list('Branch', pluck='name')
 	if event in ["on_change","on_update","after_insert"]:
@@ -53,6 +60,13 @@ def delete_synced_record(name):
 	frappe.db.sql("""DELETE FROM `tabData for Sync` WHERE branch = 'Stores - LA'""")
 	frappe.delete_doc('Data for Sync', name)
 	frappe.db.commit()
+
+@frappe.whitelist()
+def delete_data_for_sync_record(branch_name):
+	frappe.db.sql("""DELETE FROM `tabData for Sync` WHERE branch = 'Stores - LA'""")
+	frappe.db.sql("""DELETE FROM `tabData for Sync` WHERE branch = %s""",(branch_name))
+	
+
 
 
 @frappe.whitelist()
