c59f641eaca0b1bc5a804a03f80dabe57352e1ff
frappeerpnext <frapperpnext@gmail.com>
2022-09-09 11:31:32 +0700

    update

--- frappe/custom/report/daily_sale_transaction/daily_sale_transaction.py
+++ frappe/custom/report/daily_sale_transaction/daily_sale_transaction.py
@@ -11,14 +11,15 @@ def execute(filters=None):
 	return get_column, get_data,[],[],get_summary(filters) ,True
 
 def get_filters(filters):
-	con = "b.posting_date BETWEEN '{0}' AND '{1}' AND b.company = '{2}' and a.docstatus=1 and b.docstatus=1".format(filters.start_date,filters.end_date,filters.company)
+	con = "b.posting_date BETWEEN '{0}' AND '{1}' AND b.company = '{2}' and b.docstatus=1".format(filters.start_date,filters.end_date,filters.company)
 	if filters.get("price_list"): con = con + " and b.selling_price_List in (" + get_list(filters,"price_list") + ")"
 	if filters.get("customer"):con = con +	" and b.customer in (" + get_list(filters,"customer") + ")"
 	return con
 
 def get_summary(filters):
 	sql = """
-		SELECT
+		WITH sale_data AS(SELECT 
+			a.parent name,
 			count(distinct(b.customer)) customer,
 			count(DISTINCT(a.parent)) parent,
 			sum(a.qty) qty,
@@ -26,22 +27,38 @@ def get_summary(filters):
 			sum(a.amount) sub_total,
 			sum(a.amount) - SUM(a.net_amount) discount,
 			SUM(a.net_amount) grand_total,
-			SUM(a.net_amount) - SUM(coalesce(a.incoming_rate,0)*a.qty) profit,
-			SUM(b.paid_amount) paid_amount,
-			sum(a.net_amount-b.paid_amount) balance
-		FROM `tabSales Invoice Item` a
-		INNER JOIN `tabSales Invoice` b ON b.name = a.parent
+			SUM(a.net_amount) - SUM(coalesce(a.incoming_rate,0)*a.qty) profit
+		FROM `tabSales Invoice Item` a 
+			INNER JOIN `tabSales Invoice` b ON b.name = a.parent 
+			INNER JOIN `tabCustomer` c ON c.name = b.customer 
 		WHERE {0}
+			GROUP BY a.parent)
+			,paid_amount AS(
+			SELECT 
+				name,
+				paid_amount
+			FROM `tabSales Invoice` b
+			WHERE {0} 
+			GROUP BY name)
+			SELECT 
+				count(customer) customer,
+				count(parent) parent,
+				sum(qty) qty,
+				sum(cost) cost,
+				sum(sub_total) sub_total,
+				sum(discount) discount,
+				sum(grand_total) grand_total,
+				sum(profit) profit,
+				sum(b.paid_amount) paid_amount,
+				sum(a.grand_total - b.paid_amount) balance
+			FROM sale_data a
+			INNER JOIN paid_amount b ON b.name = a.name
 	""".format(get_filters(filters))
 	data=[]
 	sq = frappe.db.sql(sql,as_dict=1)
 	if sq[0]["customer"] != 0:
-		data.append({"label":"Customer","value":"{:,.0f}".format(sq[0]["customer"])})
-		data.append({"label":"Invoice","value":"{:,.0f}".format(sq[0]["parent"])})
 		data.append({"label":"Total QTY","value":"{:,.0f}".format(sq[0]["qty"])})
 		data.append({"label":"Total Cost","value":"$ {:,.4f}".format(sq[0]["cost"])})
-		data.append({"label":"Sub Total","value":"$ {:,.4f}".format(sq[0]["sub_total"])})
-		data.append({"label":"Total Disc.","value":"$ {:,.4f}".format(sq[0]["discount"])})
 		data.append({"label":"Grand Total","value":"$ {:,.4f}".format(sq[0]["grand_total"])})
 		data.append({"label":"Total Profit","value":"$ {:,.4f}".format(sq[0]["profit"])})
 		data.append({"label":"Total Payment","value":"$ {:,.4f}".format(sq[0]["paid_amount"])})
@@ -61,6 +78,8 @@ def get_columns(filters):
 	columns.append({'fieldname':'discount','label':"Discount",'fieldtype':'Currency','align':'right','width':150})
 	columns.append({'fieldname':'grand_total','label':"Amount",'fieldtype':'Currency','align':'right','width':150})
 	columns.append({'fieldname':'profit','label':"Profit",'fieldtype':'Currency','align':'right','width':150})
+	columns.append({'fieldname':'paid_amount','label':"Payment",'fieldtype':'Currency','align':'right','width':150})
+	columns.append({'fieldname':'balance','label':"Balance",'fieldtype':'Currency','align':'right','width':150})
 	return columns
 
 def get_columns_by_customer(filters):
@@ -80,6 +99,7 @@ def get_columns_by_customer(filters):
 
 def get_report_data(filters):
 	sql = """
+		WITH sale_data AS(
 		SELECT
 			a.parent,
 			b.posting_date,
@@ -94,30 +114,54 @@ def get_report_data(filters):
 		FROM `tabSales Invoice Item` a
 		INNER JOIN `tabSales Invoice` b ON b.name = a.parent
 		WHERE {0}
-		GROUP BY a.parent,b.posting_date,b.customer,b.customer_name
+		GROUP BY a.parent,b.posting_date,b.customer,b.customer_name)
+		,paid_amount AS(
+		SELECT 
+			name,
+			paid_amount
+		FROM `tabSales Invoice` b
+		WHERE {0}
+		and docstatus=1 GROUP BY name)
+		SELECT 
+			a.*,
+			b.paid_amount,
+			a.grand_total - b.paid_amount balance
+		FROM sale_data a
+		INNER JOIN paid_amount b ON b.name = a.parent
 	""".format(get_filters(filters))
 	data = frappe.db.sql(sql,as_dict=1)
 	return data
 
 def get_report_data_group_by_customer(filters):
 	parent_sql = """
-		SELECT
-			b.customer,
-			CONCAT(b.customer_name," / ",coalesce(c.phone_number,"")) as customer_name,
-			count(DISTINCT(a.parent)) parent,
-			sum(a.qty) qty,
-			SUM(COALESCE(a.incoming_rate,0)*a.qty) cost,
-			sum(a.amount) sub_total,
-			sum(a.amount) - SUM(a.net_amount) discount,
-			SUM(a.net_amount) grand_total,
-			SUM(a.net_amount) - SUM(coalesce(a.incoming_rate,0)*a.qty) profit,
-			SUM(b.paid_amount) paid_amount,
-			sum(a.net_amount-b.paid_amount) balance
-		FROM `tabSales Invoice Item` a
-		INNER JOIN `tabSales Invoice` b ON b.name = a.parent
-		INNER JOIN `tabCustomer` c ON c.name = b.customer
-		WHERE {0}
-		GROUP BY b.customer,b.customer_name,c.phone_number
+			WITH sale_data AS(
+				SELECT 
+					b.customer, 
+					CONCAT(b.customer_name," / ",coalesce(c.phone_number,"")) as customer_name, 
+					count(DISTINCT(a.parent)) parent, 
+					sum(a.qty) qty, 
+					SUM(COALESCE(a.incoming_rate,0)*a.qty) cost, 
+					sum(a.amount) sub_total, sum(a.amount) - SUM(a.net_amount) discount, 
+					SUM(a.net_amount) grand_total, 
+					SUM(a.net_amount) - SUM(coalesce(a.incoming_rate,0)*a.qty) profit
+				FROM `tabSales Invoice Item` a 
+				INNER JOIN `tabSales Invoice` b ON b.name = a.parent 
+				INNER JOIN `tabCustomer` c ON c.name = b.customer 
+				WHERE {0}
+			GROUP BY b.customer,b.customer_name,c.phone_number)
+				,paid_amount AS(
+				SELECT 
+					customer,
+					paid_amount
+				FROM `tabSales Invoice` b
+				WHERE {0}
+				GROUP BY customer)
+				SELECT 
+					a.*,
+					b.paid_amount,
+					a.grand_total - b.paid_amount balance
+				FROM sale_data a
+				INNER JOIN paid_amount b ON b.customer = a.customer
 	""".format(get_filters(filters))
 	data=[]
 	parent = frappe.db.sql(parent_sql,as_dict=1)
@@ -126,7 +170,7 @@ def get_report_data_group_by_customer(filters):
 		dic_p["is_group"]=1
 		data.append(dic_p)
 		child = frappe.db.sql("""
-								SELECT
+								WITH sale_data AS(SELECT
 									a.parent,
 									b.posting_date,
 									sum(a.qty) qty,
@@ -134,13 +178,24 @@ def get_report_data_group_by_customer(filters):
 									sum(a.amount) sub_total,
 									sum(a.amount) - SUM(a.net_amount) discount,
 									SUM(a.net_amount) grand_total,
-									SUM(a.net_amount) - SUM(coalesce(a.incoming_rate,0)*a.qty) profit,
-									SUM(paid_amount) paid_amount,
-									sum(a.net_amount-b.paid_amount) balance
+									SUM(a.net_amount) - SUM(coalesce(a.incoming_rate,0)*a.qty) profit
 								FROM `tabSales Invoice Item` a
 								INNER JOIN `tabSales Invoice` b ON b.name = a.parent
 								WHERE {0} and b.customer = '{1}'
-								GROUP BY a.parent,b.posting_date,b.customer_name,b.customer
+								GROUP BY a.parent,b.posting_date,b.customer_name,b.customer)
+								,paid_amount AS(
+								SELECT 
+									name,
+									paid_amount
+								FROM `tabSales Invoice` b
+								WHERE {0} and b.customer = '{1}'
+								and docstatus=1 GROUP BY name)
+								SELECT 
+									a.*,
+									b.paid_amount,
+									a.grand_total - b.paid_amount balance
+								FROM sale_data a
+								INNER JOIN paid_amount b ON b.name = a.parent
 							""".format(get_filters(filters),dic_p["customer"]), as_dict=1)
 		for dic_c in child:
 			dic_c["indent"] = 1
