5ff03553e3ef0e51c4d587d3b2f921e00bf82ab3
frappeerpnext <frapperpnext@gmail.com>
2023-10-21 09:55:40 +0000

    update 2023-10-21 4:55 PM

--- frappe/custom/doctype/item_list/item_list.js
+++ frappe/custom/doctype/item_list/item_list.js
@@ -2,7 +2,34 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Item List', {
-	// refresh: function(frm) {
-
-	// }
+	purchase_order:function(frm){
+		frm.clear_table("items");
+		frm.doc.purchase_invoice = ""
+		frm.refresh_field('purchase_invoice');
+		frappe.model.with_doc("Purchase Order", frm.doc.purchase_order, function() {
+            var tabletransfer= frappe.model.get_doc("Purchase Order", frm.doc.purchase_order)
+            $.each(tabletransfer.items, function(index, row){
+				frm.add_child('items', {
+					item: row.item_code,
+					copies: 1
+					});
+				frm.refresh_field('items');
+            });
+        });
+	},
+	purchase_invoice:function(frm){
+		frm.clear_table("items");
+		frm.doc.purchase_order = ""
+		frm.refresh_field('purchase_order');
+		frappe.model.with_doc("Purchase Invoice", frm.doc.purchase_invoice, function() {
+            var tabletransfer= frappe.model.get_doc("Purchase Invoice", frm.doc.purchase_invoice)
+            $.each(tabletransfer.items, function(index, row){
+				frm.add_child('items', {
+					item: row.item_code,
+					copies: 1
+					});
+				frm.refresh_field('items');
+            });
+        });
+	}
 });
